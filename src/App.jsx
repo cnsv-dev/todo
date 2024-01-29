4ac3e4ae5f4147c4cc3c216066a89c963b0bb0a1
Paul T. <45326403+katistix@users.noreply.github.com>
2024-01-29 12:55:24 +0000

    anaf 1990

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,21 @@
 import './App.css';
-import { useState } from 'react';
 
 
+function ElementLista(){
+  return(
+
+    <div className='elementLista'>
+    <p className='textLista'>dadada</p>
+    <button className='Butonescu'>ok</button>
+    </div>
+  )
+}
 function App() {
-const [text, setText]= useState("")
-const [lista, setLista] = useState([])
-
-function Add() {
-  setLista([...lista, text])
-}
-
-function sterge(elem){
-  const noualista=lista.filter((el)=> {
-    if(el==elem){
-      return 0;
-    }
-    else{
-      return 1;
-    }
-  })
-  setLista(noualista)
-}
 
   return (
-    <div>
-        <input 
-          placeholder='Scrie cv...' 
-          value={text}
-          onChange={(e)=>setText(e.target.value)}></input>
-        <button onClick={Add}>ADD</button>
-        <h1>{text}</h1>
-        <ul>
-          {
-            lista.map((el) => {
-              return (
-                <li>
-                  <button onClick={()=>sterge(el)}>-</button>
-                  {el}</li>
-              )
-            })
-          }
-        </ul>
+    <div className='lista'>
+      <ElementLista/>
+      <ElementLista/>
     </div>
   );
 }
